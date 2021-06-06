b74bda4932b069ed5d72dd5a915e57d1d917ebf4
developer4u <38066982+developer4u@users.noreply.github.com>
2021-06-06 18:18:48 +0500

    changes

---
+++ .history/src/views/FAQ_20210606124056.vue
@@ -0,0 +1,130 @@
+<template>
+  <main>
+o    <section class="faq-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="faq-content text-center">
+              <img src="@/assets/images/faq.png" />
+              <h2>Find your answers</h2>
+              <p>
+                in our FAQ list or
+                <router-link to="/contact-us">Contact Us</router-link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="faq-content">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="accordion_container">
+              
+              <FAQAccordion />
+
+              <div class="faq-vector faq-vector1">
+                <img src="@/assets/images/faq-vector.png" />
+              </div>
+              <div class="faq-vector faq-vector2">
+                <img src="@/assets/images/faq-vector1.png" />
+              </div>
+              <div class="faq-vector faq-vector3">
+                <img src="@/assets/images/faq-vector2.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+
+<script>
+import DefaultLayout from "@/components/layouts/DefaultLayout.vue";
+import FAQAccordion from "../components/FAQAccordion.vue";
+// import AccordionItem from "@/components/AccordionItem.vue";
+
+export default {
+  name: "FAQ",
+  components: {FAQAccordion },
+
+  data() {
+    return {
+      accordionItems: [
+        // {
+        //   title: "How does Shortlyst differ from other tools?",
+        //   contents:
+        //     "In Shortlyst we pride ourselves in being every recruiter’s handy tool. We want you to hire the best team with the least amount of effort required. Hence we do not hide any talents from our database in your search results! We even help you automate your efforts in building relationships with the talents you want.",
+        // },
+        // {
+        //   title: "How do I manage my Account and Subscription?",
+        //   contents:
+        //     "<p>For Shortlyst Pro accounts:</p> <p>You can go to Settings to manage your account and subscription. To upgrade to Shortlyst Enterprise, please <a href='contact-sales' onclick='event.preventDefault(); app._router.push('/contact-sales/');'/>Contact Sales</a></p> <p>For Shortlyst Enterprise accounts:</p> <p>To make changes to your account and subscription, please <a href='contact-sales' onclick='event.preventDefault(); app._router.push('/contact-sales/');'/>Contact Sales</a></p>",
+        // },
+        // {
+        //   title: "How many profiles does Shortlyst have?",
+        //   contents:
+        //     "Shortlyst has aggregated 550M+ profiles globally from various online sources. Our team is committed to grow this number as we continuously update and aggregate from additional online sources",
+        // },
+        // {
+        //   title: "What are the main features on Shortlyst?",
+        //   contents:
+        //     "The following are the key features we would like to highlight from our product:<br/>•	AI talent recommendations <br/>•	Automated reaching out<br/>•	Collaboration features <br/>•	Contact information reveal<br/>•	Smart inbox",
+        // },
+        // {
+        //   title: "Can I integrate with my Applicant Tracking System (ATS)?",
+        //   contents:
+        //     "With Shortlyst Enterprise plan, we provide the option for you to integrate with your ATS. For more information about integrations, please contact your dedicated account manager or Contact Us. ",
+        // },
+        // {
+        //   title: "How does Shortlyst protect users’ data?",
+        //   contents:
+        //     "Please refer to our detailed <router-link to='privacy'>Privacy Policy</router-link> to address data protection and privacy.<br />For further details, you can contact your dedicated account manager or Contact Us",
+        // },
+        // {
+        //   title: "How can I get in touch?",
+        //   contents:
+        //     "c",
+        // },
+        //OLD ONE
+        // {
+        //   title: "What is a contact credit?",
+        //   contents:
+        //     "A contact credit is used to reveal premium information such as contact details of talent profiles. 1 contact credit = 1 profile contact reveal A credit will be deducted from your quota each time you click on the hidden contact and it will remain unlocked for future sessions. ",
+        // },
+        // {
+        //   title: "What are the differences between plans?",
+        //   contents:
+        //     "Each pricing plan offers a different number of features and contact credits tailored to different needs. For further explanation on the difference, please refer to the pricing above to see what features and contact credits are provided for different packages.",
+        // },
+        // {
+        //   title: "Will my plan renew automatically?",
+        //   contents:
+        //     "Your plan renews automatically based on your billing plan (monthly or annually). Your monthly or annual contact credits will also be issued automatically.",
+        // },
+        // {
+        //   title: "What happens to unused contact credits?",
+        //   contents:
+        //     "Any credits obtained through our subscription plan does not roll over to the next month. Additional purchase of credits will roll over to the next month and you will be able to use it for up to 90 days. We always deplete credits from your subscription plan first before deducting from your purchased credits. ",
+        // },
+        // {
+        //   title: "Can I cancel my subscription?",
+        //   contents:
+        //     "For Shortlyst Pro accounts:<br/>You can cancel your subscription at any time. To cancel, go to Settings and then to the Subscription page. To upgrade to Shortlyst Enterprise, please Contact Sales. *links to Landing-Page-Request Demo-2*",
+        // },
+      ],
+    };
+  },
+  created() {
+    this.$emit(`update:layout`, DefaultLayout);
+  },
+  mounted() {
+    //
+  },
+};
+</script>
+
+</style>
---
+++ .history/src/views/Pricing_20210606170824.vue
@@ -0,0 +1,412 @@
+<template>
+  <main>
+    <section class="prive-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="price-title text-center">
+              <div class="price-vect prices-vector">
+                <img src="@/assets/images/price-vector6.png" />
+              </div>
+              <div class="price-vect1 prices-vector">
+                <img src="@/assets/images/price-vector9.png" />
+              </div>
+              <div class="price-vect2 prices-vector">
+                <img src="@/assets/images/price-vecotr1.png" />
+              </div>
+              <div class="price-vect3 prices-vector">
+                <img src="@/assets/images/price-vector2.png" />
+              </div>
+              <div class="price-vect4 prices-vector">
+                <img src="@/assets/images/price-vector4.png" />
+              </div>
+              <h2>
+                Find a plan that<br />
+                best fits you
+              </h2>
+               <div class="bill-annualy">
+                <h6 class="monthly-pay">Bill monthly</h6>
+                <label class="switch">
+                  <input type="checkbox" id="togBtn" />
+                  <div class="slider round"></div>
+                </label>
+                <h6 class="annually-pay">Bill annually</h6>
+                <img src="@/assets/images/save-20.png" class="save-imgs" />
+              </div> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="pricing-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="pricing-content">
+              <div class="pricing-head">
+                <h5>Professional</h5>
+                <!-- <p>/Month</p> -->
+              </div>
+              <div class="pricing-body">
+                <ul>
+                  <li>
+                    <img
+                      src="@/assets/images/green-check.png"
+                      class="mr-2"
+                    />550M+ database access
+                  </li>
+                  <li>
+                    <img src="@/assets/images/green-check.png" class="mr-2" />AI
+                    Search engine
+                    <img src="@/assets/images/info.png" class="ml-2" />
+                  </li>
+                  <li>
+                    <img
+                      src="@/assets/images/green-check.png"
+                      class="mr-2"
+                    />Smart inbox
+                    <img src="@/assets/images/info.png" class="ml-2" />
+                  </li>
+                  <li>
+                    <img
+                      src="@/assets/images/green-check.png"
+                      class="mr-2"
+                    />100 Linkedin reach outs/week
+                  </li>
+                  <li>
+                    <img src="@/assets/images/green-check.png" class="mr-2" />50
+                    contact reveal/month
+                  </li>
+                  <li>
+                    <img src="@/assets/images/green-check.png" class="mr-2" />10
+                    active jobs
+                  </li>
+                </ul>
+              </div>
+              <div class="pricing-footer">
+              <button type="button" style="pointer-events:none;" class="btn btn-green">Coming Soon</button>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="pricing-content">
+              <div class="content-vector">
+                <img src="@/assets/images/price-vector11.png" />
+              </div>
+              <div class="pricing-head">
+                <h5>Enterprise</h5>
+                <h4>Custom</h4>
+              </div>
+              <div class="pricing-body">
+                <ul>
+                  <li>Everything in Pro, plus</li>
+                  <li>
+                    <img
+                      src="@/assets/images/blue-check.png"
+                      class="mr-2"
+                    />Unlimited sourcing
+                  </li>
+                  <li>
+                    <img
+                      src="@/assets/images/blue-check.png"
+                      class="mr-2"
+                    />Unlimited active jobs
+                  </li>
+                  <li>
+                    <img
+                      src="@/assets/images/blue-check.png"
+                      class="mr-2"
+                    />Collaboration features
+                  </li>
+
+                  <li>
+                    <img src="@/assets/images/blue-check.png" class="mr-2" />
+                    100 contact reveal/month
+                  </li>
+                  <li>
+                    <img
+                      src="@/assets/images/blue-check.png"
+                      class="mr-2"
+                    />Dedicated account manager
+                  </li>
+                </ul>
+              </div>
+              <div class="pricing-footer">
+                <a href="#" class="btn btn-blue-sales">Contact Sales</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="brand-trust our-brand">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="main-title text-center">
+              <h4>Some of our happy customers</h4>
+            </div>
+          </div>
+        </div>
+        <div class="brand-slider brand-images">
+          <div
+              class="brand-img"
+            >
+              <img src="@/assets/images/pricing1_logo.png" class="fixed-size-img" />
+            </div>
+            <div
+              class="brand-img"
+            >
+              <img src="@/assets/images/pricing2_logo.png" class="fixed-size-img" />
+            </div>
+            <div
+              class="brand-img"
+            >
+              <img src="@/assets/images/pricing3_logo.png" class="fixed-size-img" />
+            </div>
+            <div
+              class="brand-img"
+            >
+              <img src="@/assets/images/pricing4_logo.png" class="fixed-size-img" />
+            </div>
+            <div
+              class="brand-img"
+            >
+              <img src="@/assets/images/pricing5_logo.png" class="fixed-size-img" />
+            </div>
+            <div
+              class="brand-img"
+            >
+              <img src="@/assets/images/pricing6_logo.png" class="fixed-size-img" />
+            </div>
+            <div
+              class="brand-img"
+            >
+              <img src="@/assets/images/pricing7_logo.png" class="fixed-size-img" />
+            </div>
+            <div
+              class="brand-img"
+            >
+              <img src="@/assets/images/pricing8_logo.png" class="fixed-size-img" />
+            </div>
+            <div
+              class="brand-img"
+            >
+              <img src="@/assets/images/pricing9_logo.png" class="fixed-size-img" />
+            </div>
+        </div>
+      </div>
+    </section>
+    <section class="pricing-faq">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="faq-content text-center">
+              <h2>Questions you might have</h2>
+              <p>Need more help? Check out our <a href="#"> FAQ</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="pricing-faqs">
+              <div class="vector1">
+                <img src="@/assets/images/faq.png" />
+              </div>
+              <div class="vector2">
+                <img src="@/assets/images/vector2.png" />
+              </div>
+              <div class="vector3">
+                <img src="@/assets/images/vector.png" />
+              </div>
+
+              <accordion :items="accordionItems"></accordion>
+
+              <!--              
+
+              <div class="accordian-tabs">
+                <div class="accordion_head">
+                  <h5>
+                    How does Shortlyst differ from other tools?<span
+                      class="plusminus"
+                      ><img src="@/assets/images/chevron_big_down.png"
+                    /></span>
+                  </h5>
+                </div>
+                <div class="accordion_body" style="display: none">
+                  <p>
+                    In Shortlyst we pride ourselves in being every recruiter’s
+                    handy tool. We want you to hire the best team with the least
+                    amount of effort required. Hence we do not hide any talents
+                    from our database in your search results! We even help you
+                    automate your efforts in building relationships with the
+                    talents you want.
+                  </p>
+                </div>
+              </div>
+              <div class="accordian-tabs">
+                <div class="accordion_head">
+                  <h5>
+                    What is a contact credit?<span class="plusminus"
+                      ><img src="@/assets/images/chevron_big_down.png"
+                    /></span>
+                  </h5>
+                </div>
+                <div class="accordion_body" style="display: none">
+                  <p>
+                    A contact credit is used to reveal premium information such
+                    as contact details of talent profiles. 1 contact credit = 1
+                    profile contact reveal A credit will be deducted from your
+                    quota each time you click on the hidden contact and it will
+                    remain unlocked for future sessions.
+                  </p>
+                </div>
+              </div>
+              <div class="accordian-tabs">
+                <div class="accordion_head">
+                  <h5>
+                    What are the differences between plans?<span
+                      class="plusminus"
+                      ><img src="@/assets/images/chevron_big_down.png"
+                    /></span>
+                  </h5>
+                </div>
+                <div class="accordion_body" style="display: none">
+                  <p>
+                    Each pricing plan offers a different number of features and
+                    contact credits tailored to different needs. For further
+                    explanation on the difference, please refer to the pricing
+                    above to see what features and contact credits are provided
+                    for different packages.
+                  </p>
+                </div>
+              </div>
+              <div class="accordian-tabs">
+                <div class="accordion_head">
+                  <h5>
+                    Will my plan renew automatically?<span class="plusminus"
+                      ><img src="@/assets/images/chevron_big_down.png"
+                    /></span>
+                  </h5>
+                </div>
+                <div class="accordion_body" style="display: none">
+                  <p>
+                    Your plan renews automatically based on your billing plan
+                    (monthly or annually). Your monthly or annual contact
+                    credits will also be issued automatically.
+                  </p>
+                </div>
+              </div>
+              <div class="accordian-tabs">
+                <div class="accordion_head">
+                  <h5>
+                    What happens to unused contact credits?<span
+                      class="plusminus"
+                      ><img src="@/assets/images/chevron_big_down.png"
+                    /></span>
+                  </h5>
+                </div>
+                <div class="accordion_body" style="display: none">
+                  <p>
+                    Any credits obtained through our subscription plan does not
+                    roll over to the next month. Additional purchase of credits
+                    will roll over to the next month and you will be able to use
+                    it for up to 90 days. We always deplete credits from your
+                    subscription plan first before deducting from your purchased
+                    credits.
+                  </p>
+                </div>
+              </div>
+              <div class="accordian-tabs">
+                <div class="accordion_head">
+                  <h5>
+                    Can I cancel my subscription?<span class="plusminus"
+                      ><img src="@/assets/images/chevron_big_down.png"
+                    /></span>
+                  </h5>
+                </div>
+                <div class="accordion_body" style="display: none">
+                  <p>
+                    For Shortlyst Pro accounts:<br />
+                    You can cancel your subscription at any time. To cancel, go
+                    to Settings and then to the Subscription page. To upgrade to
+                    Shortlyst Enterprise, please Contact Sales. *links to
+                    Landing-Page-Request Demo-2*
+                  </p>
+                </div>
+              </div> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import DefaultLayout from "@/components/layouts/DefaultLayout.vue";
+import Accordion from "@/components/Accordion.vue";
+
+export default {
+  name: "Pricing",
+  components: { Accordion },
+  data() {
+    return {
+      accordionItems: [
+        {
+          title: "What are the payment options?",
+          contents:
+            "We have 2 pricing plans - Shortlyst Pro and Shortlyst Enterprise. We offer monthly and annually billing options for paid plans. The monthly billing plan gives you the flexibility to pay month-to-month. The annual billing plan is a discounted option that requires an upfront payment for an entire year. For more details, please Contact Sales.",
+        },
+        {
+          title: "What is a contact credit?",
+          contents:
+            "A contact credit is used to reveal premium information such as contact details of talent profiles. 1 contact credit = 1 profile contact reveal                                         A credit will be deducted from your quota each time you click on the hidden contact and it will remain unlocked for future sessions.",
+        },
+        {
+          title: "What are the differences between plans?",
+          contents:
+            "Each pricing plan offers a different number of features and contact credits tailored to different needs. For further explanation on the difference, please refer to the pricing above to see what features and contact credits are provided for different packages.",
+        },
+        {
+          title: "Will my plan renew automatically?",
+          contents:
+            "Your plan renews automatically based on your billing plan (monthly or annually). Your monthly or annual contact credits will also be issued automatically.",
+        },
+        {
+          title: "What happens to unused contact credits?",
+          contents:
+            "Any credits obtained through our subscription plan does not roll over to the next month. Additional purchase of credits will roll over to the next month and you will be able to use it for up to 90 days. We always deplete credits from your subscription plan first before deducting from your purchased credits.",
+        },
+        {
+          title: "Can I cancel my subscription?",
+          contents:
+            "For Shortlyst Pro accounts:<br /> You can cancel your subscription at any time. To cancel, go to Settings and then to the Subscription page. To upgrade to Shortlyst Enterprise, please Contact Sales. *links to Landing-Page-Request Demo-2*",
+        },
+      ],
+    };
+  },
+  created() {
+    this.$emit(`update:layout`, DefaultLayout);
+  },
+  mounted() {},
+};
+$( document ).ready(function() {
+  $('.bill-annualy').addClass('pay_monthly');
+  $('#').click(function(){
+      if($(this).prop("checked") == true){
+          console.log("Checkbox is checked.");
+      }
+      else if($(this).prop("checked") == false){
+          console.log("Checkbox is unchecked.");
+      }
+  });
+});
+</script>
+
+<style>
+.prive-banner {
+  padding-top: 250px;
+}
+</style>
